402cb5695eda648cb76c4c9df42b2ba019aed52e
Martin Piatka <445597@mail.muni.cz>
2017-06-14 13:59:32 +0200

    NV12 SSE test

--- src/video_compress/libavcodec.cpp
+++ src/video_compress/libavcodec.cpp
@@ -1029,7 +1029,64 @@ static void to_nv12(AVFrame *out_frame, unsigned char *in_data, int width, int h
                 unsigned char *dst_y = out_frame->data[0] + out_frame->linesize[0] * y;
                 unsigned char *dst_y2 = out_frame->data[0] + out_frame->linesize[0] * (y + 1);
                 unsigned char *dst_cbcr = out_frame->data[1] + out_frame->linesize[1] * y / 2;
-                for(int x = 0; x < width / 2; ++x) {
+
+				int x = 0;
+				__m128i yuv;
+				__m128i yuv2;
+				__m128i y1;
+				__m128i y2;
+				__m128i y3;
+				__m128i y4;
+				__m128i uv;
+				__m128i uv2;
+				__m128i uv3;
+				__m128i uv4;
+				__m128i ymask = _mm_set1_epi32(0x00FF00FF);
+				__m128i dsty;
+				__m128i dsty2;
+				__m128i dstuv;
+#if 1
+				while (x < (width / 8)){
+					yuv = _mm_lddqu_si128((__m128i const*) src);
+					yuv2 = _mm_lddqu_si128((__m128i const*) src2);
+					src += 16;
+					src2 += 16;
+
+					y1 = _mm_and_si128(ymask, yuv);
+					y2 = _mm_and_si128(ymask, yuv2);
+
+					uv = _mm_andnot_si128(ymask, yuv);
+					uv2 = _mm_andnot_si128(ymask, yuv2);
+
+					uv = _mm_avg_epu8(uv, uv2);
+
+					yuv = _mm_lddqu_si128((__m128i const*) src);
+					yuv2 = _mm_lddqu_si128((__m128i const*) src2);
+					src += 16;
+					src2 += 16;
+
+					y3 = _mm_and_si128(ymask, yuv);
+					y4 = _mm_and_si128(ymask, yuv2);
+
+					uv3 = _mm_andnot_si128(ymask, yuv);
+					uv4 = _mm_andnot_si128(ymask, yuv2);
+
+					uv3 = _mm_avg_epu8(uv, uv2);
+
+					dsty = _mm_packus_epi16(y1, y3);
+					dsty2 = _mm_packus_epi16(y2, y4);
+					dstuv = _mm_packus_epi16(uv, uv3);
+					_mm_storeu_si128((__m128i *) dst_y, dsty);
+					_mm_storeu_si128((__m128i *) dst_y2, dsty2);
+					_mm_storeu_si128((__m128i *) dst_cbcr, dstuv);
+					dst_y += 16;
+					dst_y2 += 16;
+					dst_cbcr += 16;
+					x++;
+				}
+#endif
+#if 1
+                for(x = x * 4; x < width / 2; ++x) {
                         *dst_cbcr++ = (*src++ + *src2++) / 2;
                         *dst_y++ = *src++;
                         *dst_y2++ = *src2++;
@@ -1037,6 +1094,7 @@ static void to_nv12(AVFrame *out_frame, unsigned char *in_data, int width, int h
                         *dst_y++ = *src++;
                         *dst_y2++ = *src2++;
                 }
+#endif
         }
 }
 
