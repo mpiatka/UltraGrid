979d1fc29f2ed8bee841d9fbc24f362b5a32c1fd
Martin Piatka <445597@mail.muni.cz>
2018-01-23 13:50:35 +0100

    hwaccel: Funcs for creating ug frames from av frames

--- src/hwaccel.c
+++ src/hwaccel.c
@@ -1,25 +1,26 @@
 #include "hwaccel.h"
 
 void hw_vdpau_ctx_init(hw_vdpau_ctx *ctx){
-        device_ref = NULL;
-        device = NULL;
-        get_proc_address = NULL;
+        ctx->device_ref = NULL;
+        ctx->device = 0;
+        ctx->get_proc_address = NULL;
 }
 
 void hw_vdpau_ctx_unref(hw_vdpau_ctx *ctx){
         av_buffer_unref(&ctx->device_ref);
 
-        ctx->device = NULL;
-        ctx->get_proc_address = NULL;
+        hw_vdpau_ctx_init(ctx);
 }
 
-hw_vdpau_ctx hw_vdpau_ctx_copy(hw_vdpau_ctx *ctx){
+hw_vdpau_ctx hw_vdpau_ctx_copy(const hw_vdpau_ctx *ctx){
         hw_vdpau_ctx new_ctx;
         hw_vdpau_ctx_init(&new_ctx);
 
         new_ctx.device_ref = av_buffer_ref(ctx->device_ref);
-        new.device = ctx->device;
-        new.get_proc_address = ctx->get_proc_address;
+        new_ctx.device = ctx->device;
+        new_ctx.get_proc_address = ctx->get_proc_address;
+
+        return new_ctx;
 }
 
 void hw_vdpau_frame_init(hw_vdpau_frame *frame){
@@ -36,19 +37,18 @@ void hw_vdpau_frame_init(hw_vdpau_frame *frame){
 void hw_vdpau_frame_unref(hw_vdpau_frame *frame){
         for(int i = 0; i < AV_NUM_DATA_POINTERS; i++){
                 av_buffer_unref(&frame->buf[i]);
-                frame->data[i] = NULL;
         }
 
-        frame->surface = NULL;
-
         hw_vdpau_ctx_unref(&frame->hwctx);
+
+        hw_vdpau_frame_init(frame);
 }
 
 hw_vdpau_frame hw_vdpau_frame_copy(const hw_vdpau_frame *frame){
         hw_vdpau_frame new_frame;
-        hw_vdpau_frame_init(&frame);
+        hw_vdpau_frame_init(&new_frame);
 
-        new_frame.hwctx = hw_vdpau_ctx_copy(frame->hwctx);
+        new_frame.hwctx = hw_vdpau_ctx_copy(&frame->hwctx);
 
         for(int i = 0; i < AV_NUM_DATA_POINTERS; i++){
                 new_frame.buf[i] = av_buffer_ref(frame->buf[i]);
@@ -56,4 +56,28 @@ hw_vdpau_frame hw_vdpau_frame_copy(const hw_vdpau_frame *frame){
         }
 
         new_frame.surface = frame->surface;
+
+        return new_frame;
+}
+
+hw_vdpau_frame *hw_vdpau_frame_from_avframe(hw_vdpau_frame *dst, const AVFrame *src){
+        hw_vdpau_frame_init(dst);
+
+        AVHWFramesContext *frame_ctx = (AVHWFramesContext *) src->hw_frames_ctx->data;
+        AVHWDeviceContext *device_ctx = frame_ctx->device_ctx; 
+        AVVDPAUDeviceContext *vdpau_ctx = (AVVDPAUDeviceContext *) device_ctx->hwctx;
+
+
+        dst->hwctx.device_ref = av_buffer_ref(frame_ctx->device_ref);
+        dst->hwctx.device = vdpau_ctx->device;
+        dst->hwctx.get_proc_address = vdpau_ctx->get_proc_address;
+
+        for(int i = 0; i < AV_NUM_DATA_POINTERS; i++){
+                dst->buf[i] = av_buffer_ref(src->buf[i]);
+                dst->data[i] = src->data[i];
+        }
+
+        dst->surface = (VdpVideoSurface *) dst->data[3];
+
+        return dst;
 }
--- src/hwaccel.h
+++ src/hwaccel.h
@@ -7,30 +7,32 @@
 #include <libavcodec/vdpau.h>
 #include <libavcodec/vaapi.h>
 
-struct hw_vdpau_ctx{
+typedef struct hw_vdpau_ctx{
         AVBufferRef *device_ref; //Av codec buffer reference
 
         //Theese are just pointers to the device_ref buffer
-        VdpDevice *device;
+        VdpDevice device;
         VdpGetProcAddress *get_proc_address;
-};
+} hw_vdpau_ctx;
 
-struct hw_vdpau_frame{
+typedef struct hw_vdpau_frame{
         hw_vdpau_ctx hwctx;
         AVBufferRef *buf[AV_NUM_DATA_POINTERS];
 
         //Theese are just pointers to the buffer
         uint8_t *data[AV_NUM_DATA_POINTERS];
         VdpVideoSurface *surface; // Same as data[3]
-};
+} hw_vdpau_frame;
 
 void hw_vdpau_ctx_init(hw_vdpau_ctx *ctx);
 void hw_vdpau_ctx_unref(hw_vdpau_ctx *ctx);
-hw_vdpau_ctx hw_vdpau_ctx_copy(hw_vdpau_ctx *ctx);
+hw_vdpau_ctx hw_vdpau_ctx_copy(const hw_vdpau_ctx *ctx);
 
 void hw_vdpau_frame_init(hw_vdpau_frame *frame);
 void hw_vdpau_frame_unref(hw_vdpau_frame *frame);
 hw_vdpau_frame hw_vdpau_frame_copy(const hw_vdpau_frame *frame);
+
+hw_vdpau_frame *hw_vdpau_frame_from_avframe(hw_vdpau_frame *dst, const AVFrame *src);
 
 
 #endif
