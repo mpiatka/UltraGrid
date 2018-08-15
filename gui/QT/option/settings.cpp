9f2cadb123b4c98d54d1458b569ae23aa776195d
Martin Piatka <445597@mail.muni.cz>
2018-08-15 13:21:43 +0200

    Settings and Options

--- gui/QT/option/settings.cpp
+++ gui/QT/option/settings.cpp
@@ -1,9 +1,134 @@
+#include <iostream>
 #include "settings.hpp"
 
+std::string Option::getName() const{
+	return name;
+}
+
+std::string Option::getValue() const{
+	return value;
+}
+
+std::string Option::getParam() const{
+	return param;
+}
+
+static void addSpace(std::string &str){
+	if(!str.empty() && str.back() != ' '){
+		str += ' ';
+	}
+}
+
+std::string Option::getLaunchOption() const{
+	if(!enabled)
+		return "";
+
+	std::string out = param + value;
+
+	for(const auto &sub : suboptions){
+		if(sub.first.empty() || sub.first == value){
+			out += sub.second->getLaunchOption();
+		}
+	}
+
+	addSpace(out);
+
+	return out;
+}
+
+void Option::changed(){
+	for(const auto &callback : onChangeCallbacks){
+		callback(name, value);
+	}
+}
+
+void Option::setValue(const std::string &val){
+	value = val;
+	changed();
+}
+
+void Option::addSuboption(Option *sub, const std::string &limit){
+	suboptions.push_back(std::make_pair(limit, sub));
+}
+
+Settings::Settings(){
+	addOption("video.source", "-t ");
+	addOption("testcard.width", ":", "1920", true, "video.source", "testcard");
+	addOption("screen.width", ":", "1080", true, "video.source", "screen");
+
+	addOption("video.display", "-d ");
+	addOption("gl.novsync", ":", "novsync", true, "video.display", "gl");
+
+	getOption("video.source").setValue("screen");
+	getOption("video.display").setValue("gl");
+
+	std::cout << getLaunchParams() << std::endl;
+}
+
 std::string Settings::getLaunchParams() const{
-	return "";
+	std::string out;
+	out += getOption("video.source").getLaunchOption();
+	out += getOption("video.display").getLaunchOption();
+	return out;
 }
 
 std::string Settings::getPreviewParams() const{
 	return "";
+}
+
+Option& Settings::getOption(const std::string &opt){
+	auto search = options.find(opt);
+
+	if(search == options.end()){
+		std::unique_ptr<Option> newOption(new Option(opt));
+		auto p = newOption.get();
+		options[opt] = std::move(newOption);
+		return *p;
+	}
+
+	return *search->second;
+}
+
+const Option Settings::dummy;
+
+const Option& Settings::getOption(const std::string &opt) const{
+	auto search = options.find(opt);
+
+	if(search == options.end())
+		return dummy;
+
+	return *search->second;
+}
+
+Option& Settings::addOption(std::string name,
+		const std::string &param,
+		const std::string &value,
+		bool enabled,
+		const std::string &parent,
+		const std::string &limit)
+{
+	if(!parent.empty())
+		name = parent + "." + name;
+
+	auto search = options.find(name);
+
+	Option *opt;
+
+	if(search == options.end()){
+		std::unique_ptr<Option> newOption(new Option(name));
+		opt = newOption.get();
+		options[name] = std::move(newOption);
+	} else {
+		opt = search->second.get();
+	}
+
+	opt->setParam(param);
+	opt->setValue(value);
+	opt->setEnabled(enabled);
+
+	if(!parent.empty()){
+		getOption(parent).addSuboption(opt, limit);
+	}
+
+	return *opt;
 }
