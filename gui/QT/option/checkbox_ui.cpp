f1aa27a90b046a32fc2093847d39cb2d0b0fc9db
Martin Piatka <445597@mail.muni.cz>
2018-11-13 14:17:22 +0100

    checkable actions

--- gui/QT/option/checkbox_ui.cpp
+++ gui/QT/option/checkbox_ui.cpp
@@ -1,33 +1,15 @@
-#include <functional>
-
 #include "checkbox_ui.hpp"
 
 CheckboxUi::CheckboxUi(QAbstractButton *box, Settings *settings, const std::string &opt) :
-    WidgetUi(settings, opt),
+    CheckableUi(settings, opt),
     checkbox(box)
 {
     updateUiState();
-    registerCallback();
     connectSignals();
 }
 
 void CheckboxUi::connectSignals(){
 	connect(checkbox, &QAbstractButton::clicked, this, &CheckboxUi::boxClicked);
-}
-
-void CheckboxUi::boxClicked(bool checked){
-	settings->getOption(opt).setEnabled(checked);
-	emit changed();
-}
-
-void CheckboxUi::optChangeCallback(Option &changedOpt, bool suboption){
-    if(changedOpt.getName() == opt){
-        updateUiState(changedOpt.isEnabled());
-    }
-}
-
-bool CheckboxUi::getOptValue(){
-    return settings->getOption(opt).isEnabled();
 }
 
 void CheckboxUi::updateUiState(bool checked){
