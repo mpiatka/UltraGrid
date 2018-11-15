239ef6d7f741a1d4809a96411d4cee6970752495
Martin Piatka <445597@mail.muni.cz>
2018-11-15 12:38:25 +0100

    Remaining controls, remove unneeded code

--- gui/QT/option/lineedit_ui.cpp
+++ gui/QT/option/lineedit_ui.cpp
@@ -1,31 +1,15 @@
 #include "lineedit_ui.hpp"
 
 LineEditUi::LineEditUi(QLineEdit *line, Settings *settings, const std::string &opt) : 
-    WidgetUi(settings, opt),
+    TextOptUi(settings, opt),
     line(line)
 {
     updateUiState();
-    registerCallback();
     connectSignals();
 }
 
 void LineEditUi::connectSignals(){
 	connect(line, &QLineEdit::textEdited, this, &LineEditUi::textEdited);
-}
-
-void LineEditUi::textEdited(const QString &text){
-	settings->getOption(opt).setValue(text.toStdString());
-	emit changed();
-}
-
-void LineEditUi::optChangeCallback(Option &changedOpt, bool suboption){
-    if(changedOpt.getName() == opt){
-        updateUiState(changedOpt.getValue());
-    }
-}
-
-std::string LineEditUi::getOptValue(){
-    return settings->getOption(opt).getValue();
 }
 
 void LineEditUi::updateUiState(const std::string &text){
@@ -35,5 +19,4 @@ void LineEditUi::updateUiState(const std::string &text){
 void LineEditUi::updateUiState(){
     updateUiState(getOptValue());
 }
-
 
--- gui/QT/option/settings_ui.hpp
+++ gui/QT/option/settings_ui.hpp
@@ -13,6 +13,7 @@
 #include "actionCheckable_ui.hpp"
 #include "combobox_ui.hpp"
 #include "lineedit_ui.hpp"
+#include "spinbox_ui.hpp"
 #include "audio_opts.hpp"
 #include "video_opts.hpp"
 
@@ -33,38 +34,14 @@ private:
 	std::vector<std::unique_ptr<WidgetUi>> uiControls;
 
 
-	void populateComboBox(QComboBox *box,
-			SettingType type,
-			const std::vector<std::string> &whitelist = {});
-
-	void refreshVideoCompress();
-	void refreshVideoSource();
-	void refreshVideoDisplay();
-
-	void refreshAudioSource();
-	void refreshAudioPlayback();
-	void refreshAudioCompression();
-
 	void jpegLabelCallback(Option &opt, bool suboption); 
-	void videoDisplayCallback(Option &opt, bool suboption); 
-
-	void audioSourceCallback(Option &opt, bool suboption);
-	void audioPlaybackCallback(Option &opt, bool suboption);
 
 	void fecCallback(Option &opt, bool suboption);
-
-	void setComboBox(QComboBox *box, const std::string &opt, int idx);
-	void setString(const std::string &opt, const QString &str);
-	void setBool(const std::string &opt, bool b);
 
 	void connectSignals();
 	void addCallbacks();
 
 private slots:
-	void setVideoCompression(int idx);
-	void setVideoBitrate(const QString &);
-	void setVideoSourceMode(int idx);
-
 	void refreshAll();
 
 	void test();
