9564958ca920726080fc812414eb02c678020f90
Martin Piatka <445597@mail.muni.cz>
2018-12-04 14:07:55 +0100

    WIP: json parsing

--- gui/QT/option/available_settings.hpp
+++ gui/QT/option/available_settings.hpp
@@ -16,79 +16,40 @@ enum SettingType{
 	SETTING_TYPE_COUNT
 };
 
-struct VideoMode{
-	int mode_num;
-	std::string format;
-
-	enum {
-		Frame_size_dicrete,
-		Frame_size_stepwise,
-		Frame_size_cont,
-	} frame_size_type;
-
-	union {
-		struct {
-			int width;
-			int height;
-		} discrete;
-		struct {
-			int min_width;
-			int max_width;
-			int min_height;
-			int max_height;
-			int step_width;
-			int step_height;
-		} stepwise;
-	} frame_size;
-
-	enum {
-		Fps_unknown,
-		Fps_discrete,
-		Fps_stepwise,
-		Fps_cont,
-	} fps_type;
-
-	union {
-		struct {
-			long long numerator;
-			int denominator;
-		} fraction;
-
-		struct {
-			long long min_numerator;
-			int min_denominator;
-			long long max_numerator;
-			int max_denominator;
-			long long step_numerator;
-			int step_denominator;
-		} stepwise;
-	} fps;
+struct SettingVal{
+	std::string opt;
+	std::string val;
 };
 
-struct Webcam{
+struct CaptureMode{
 	std::string name;
-	std::string id;
-	std::string type;
+	std::vector<SettingVal> opts;
+};
 
-	std::vector<VideoMode> modes;
+struct Capturer{
+	std::string name;
+	std::string type;
+	std::string deviceOpt;
+
+	std::vector<CaptureMode> modes;
 };
 
 class AvailableSettings{
 public:
 	void query(const std::string &executable, SettingType type);
 	void queryCap(const QStringList &lines, SettingType type, const char *capStr);
-	void queryV4l2(const QStringList &lines);
+	void queryCapturers(const QStringList &lines);
 
 	void queryAll(const std::string &executable);
 
 	bool isAvailable(const std::string &name, SettingType type) const;
 	std::vector<std::string> getAvailableSettings(SettingType type) const;
 
-	std::vector<Webcam> getWebcams() const;
+	std::vector<Capturer> getCapturers() const;
 
 private:
 	std::vector<std::string> available[SETTING_TYPE_COUNT];
-	std::vector<Webcam> webcams;
+	std::vector<Capturer> capturers;
 
 };
 
