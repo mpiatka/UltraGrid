89817a00f4d099ae8a35cf405b4c4bd7b04e930b
Martin Piatka <445597@mail.muni.cz>
2019-12-17 14:00:48 +0100

    Added cuda RGBA to UYVY conversion

--- src/utils/cuda_pix_conv.cu
+++ src/utils/cuda_pix_conv.cu
@@ -92,6 +92,48 @@ void kern_UYVYtoRGBA(unsigned char *dst,
 	*dst_px = make_uchar4(r, g, b, 0);
 }
 
+__global__
+void kern_RGBAtoUYVY(unsigned char *dst,
+		size_t dstPitch,
+		unsigned char *src,
+		size_t srcPitch,
+		size_t width,
+		size_t height)
+{
+	const int x = (blockIdx.x * blockDim.x) + threadIdx.x;
+	const int y = (blockIdx.y * blockDim.y) + threadIdx.y;
+
+	const int uyvy_block = x;
+	const int src_x = x * 2;
+
+	if(src_x + 1 >= width)
+		return;
+
+	if(y >= height)
+		return;
+
+	uchar4 *src_px_1 = (uchar4 *) (src + y * srcPitch) + src_x;
+	uchar4 *src_px_2 = (uchar4 *) (src + y * srcPitch) + src_x + 1;
+	uchar4 *dst_px = (uchar4 *) (dst + y * dstPitch) + uyvy_block;
+
+	int y1 = 11993 * src_px_1->x + 40239 * src_px_1->y + 4063 * src_px_1->z + (1<<20);
+	int u = -6619 * src_px_1->x -22151 * src_px_1->y + 28770 * src_px_1->z;
+	int v = 28770 * src_px_1->x - 26149 * src_px_1->y - 2621 * src_px_1->z;
+
+	int y2 = 11993 * src_px_2->x + 40239 * src_px_2->y + 4063 * src_px_2->z + (1<<20);
+	u += -6619 * src_px_2->x -22151 * src_px_2->y + 28770 * src_px_2->z;
+	v += 28770 * src_px_2->x - 26149 * src_px_2->y - 2621 * src_px_2->z;
+
+	u = u / 2 + (1<<23);
+	v = v / 2 + (1<<23);
+
+	*dst_px = make_uchar4(min(max(u, 0), (1<<24)-1) >> 16,
+			min(max(y1, 0), (1<<24)-1) >> 16,
+			min(max(v, 0), (1<<24)-1) >> 16,
+			min(max(y2, 0), (1<<24)-1) >> 16);
+}
+
+
 void cuda_RGBto_RGBA(unsigned char *dst,
                 size_t dstPitch,
                 unsigned char *src,
@@ -117,6 +159,21 @@ void cuda_RGBA_to_RGB(unsigned char *dst,
 
         dim3 blockSize(32,32);
         dim3 numBlocks((width + blockSize.x - 1) / blockSize.x,
+                        (height + blockSize.y - 1) / blockSize.y);
+
+        kern_RGBAtoRGB<<<numBlocks, blockSize, 0, stream>>>(dst, dstPitch, src, srcPitch, width, height);
+}
+
+void cuda_RGBA_to_UYVY(unsigned char *dst,
+                size_t dstPitch,
+                unsigned char *src,
+                size_t srcPitch,
+                size_t width,
+                size_t height,
+                CUstream_st *stream){
+
+        dim3 blockSize(32,32);
+        dim3 numBlocks((width / 2 + blockSize.x - 1) / blockSize.x,
                         (height + blockSize.y - 1) / blockSize.y);
 
         kern_RGBAtoRGB<<<numBlocks, blockSize, 0, stream>>>(dst, dstPitch, src, srcPitch, width, height);
--- src/utils/cuda_pix_conv.h
+++ src/utils/cuda_pix_conv.h
@@ -19,6 +19,14 @@ void cuda_RGBA_to_RGB(unsigned char *dst,
                 size_t height,
                 struct CUstream_st *stream);
 
+void cuda_RGBA_to_UYVY(unsigned char *dst,
+                size_t dstPitch,
+                unsigned char *src,
+                size_t srcPitch,
+                size_t width,
+                size_t height,
+                struct CUstream_st *stream);
+
 void cuda_UYVY_to_RGBA(unsigned char *dst,
                 size_t dstPitch,
                 unsigned char *src,
