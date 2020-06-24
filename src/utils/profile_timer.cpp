959731ff5599c90d5c17ad285146a42da88a1730
Martin Piatka <445597@mail.muni.cz>
2020-06-24 11:03:16 +0200

    Profile_timer: Fix formating

--- src/utils/profile_timer.cpp
+++ src/utils/profile_timer.cpp
@@ -7,72 +7,72 @@
 std::chrono::steady_clock::time_point Profiler::start_point;
 
 Profiler& Profiler::get_instance(){
-    static Profiler inst;
-    if(inst.start_point.time_since_epoch() == std::chrono::steady_clock::duration::zero()){
-        inst.start_point = std::chrono::steady_clock::now();
-    }
-    return inst;
+        static Profiler inst;
+        if(inst.start_point.time_since_epoch() == std::chrono::steady_clock::duration::zero()){
+                inst.start_point = std::chrono::steady_clock::now();
+        }
+        return inst;
 }
 
 void Profiler::write_events(const std::vector<Profile_event>& events){
-    if(!active)
-        return;
+        if(!active)
+                return;
 
-    std::lock_guard<std::mutex> lock(io_mut);
-    std::chrono::steady_clock::time_point::rep last_end = 0;
-    for(const auto& event : events){
+        std::lock_guard<std::mutex> lock(io_mut);
+        std::chrono::steady_clock::time_point::rep last_end = 0;
+        for(const auto& event : events){
 
-            auto ts = event.start;
-            auto end = event.end;
-        out_file << "{ \"name\": \"" << event.name << "\""
-            << ", \"cat\": \"function\""
-            << ", \"ph\": \"X\""
-            << ", \"pid\": \"" << pid << "\""
-            << ", \"tid\": \"" << event.thread_id << "\""
-            << ", \"ts\": \"" << ts << "\""
-            << ", \"dur\": \"" << end - ts << "\""
-            << ", \"dbg_end\": \"" << end << "\""
-            << "},\n ";
-    }
+                auto ts = event.start;
+                auto end = event.end;
+                out_file << "{ \"name\": \"" << event.name << "\""
+                        << ", \"cat\": \"function\""
+                        << ", \"ph\": \"X\""
+                        << ", \"pid\": \"" << pid << "\""
+                        << ", \"tid\": \"" << event.thread_id << "\""
+                        << ", \"ts\": \"" << ts << "\""
+                        << ", \"dur\": \"" << end - ts << "\""
+                        << ", \"dbg_end\": \"" << end << "\""
+                        << "},\n ";
+        }
 }
 
 Profiler::Profiler(){
-    const char *env_p = std::getenv("UG_PROFILE");
-    active = env_p && env_p[0] != '\0';
-    if(!active){
-        return;
-    }
+        const char *env_p = std::getenv("UG_PROFILE");
+        active = env_p && env_p[0] != '\0';
+        if(!active){
+                return;
+        }
 
-    std::string filename = "ug_";
-    filename += env_p;
-    filename += ".json";
+        std::string filename = "ug_";
+        filename += env_p;
+        filename += ".json";
 
-    pid = std::this_thread::get_id();
-    out_file.open(filename);
+        pid = std::this_thread::get_id();
+        out_file.open(filename);
 
-    out_file << "[ ";
+        out_file << "[ ";
 }
 
 Profiler_thread_inst::Profiler_thread_inst(Profiler& profiler) : profiler(profiler) {
-    thread_events.reserve(THREAD_EVENT_BUF_SIZE);
+        thread_events.reserve(THREAD_EVENT_BUF_SIZE);
 }
 
 Profiler_thread_inst& Profiler_thread_inst::get_instance(){
-    static thread_local Profiler_thread_inst inst(Profiler::get_instance());
-    return inst;
+        static thread_local Profiler_thread_inst inst(Profiler::get_instance());
+        return inst;
 }
 
 Profiler_thread_inst::~Profiler_thread_inst(){
-    profiler.write_events(thread_events);
+        profiler.write_events(thread_events);
 }
 
 void Profiler_thread_inst::add_event(Profile_event&& event){
-    if(thread_events.size() == THREAD_EVENT_BUF_SIZE){
-        profiler.write_events(thread_events);
-        thread_events.clear();
-    }
+        if(thread_events.size() == THREAD_EVENT_BUF_SIZE){
+                profiler.write_events(thread_events);
+                thread_events.clear();
+        }
 
-    thread_events.emplace_back(std::move(event));
+        thread_events.emplace_back(std::move(event));
 }
 
 Profile_timer::Profile_timer(Profiler_thread_inst& profiler, std::string name) :
@@ -94,20 +94,20 @@ Profile_timer::Profile_timer(Profiler_thread_inst& profiler, std::string name) :
 }
 
 Profile_timer::~Profile_timer(){
-    commit();
+        commit();
 }
 
 Profile_timer& Profile_timer::operator=(Profile_timer&& rhs) {
-    std::swap(event, rhs.event);
-    return *this;
+        std::swap(event, rhs.event);
+        return *this;
 }
 
 void Profile_timer::commit(){
-    if(event.name.empty())
-        return;
+        if(event.name.empty())
+                return;
 
-    event.end = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - profiler.profiler.get_start_point()).count();
-    profiler.add_event(std::move(event));
+        event.end = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - profiler.profiler.get_start_point()).count();
+        profiler.add_event(std::move(event));
 }
 
 #endif //BUILD_PROFILED
--- src/utils/profile_timer.hpp
+++ src/utils/profile_timer.hpp
@@ -12,84 +12,84 @@
 #define THREAD_EVENT_BUF_SIZE 256
 
 #define PROFILE_FUNC \
-    Profile_timer PROFILER_PROFILE_TIMER_FUNC(Profiler_thread_inst::get_instance(), __PRETTY_FUNCTION__); \
-    Profile_timer PROFILER_PROFILE_TIMER_DETAIL(Profiler_thread_inst::get_instance(), "");
+        Profile_timer PROFILER_PROFILE_TIMER_FUNC(Profiler_thread_inst::get_instance(), __PRETTY_FUNCTION__); \
+        Profile_timer PROFILER_PROFILE_TIMER_DETAIL(Profiler_thread_inst::get_instance(), "");
 
 #define PROFILE_DETAIL(name) \
-    PROFILER_PROFILE_TIMER_DETAIL = Profile_timer(Profiler_thread_inst::get_instance(), (name));
+        PROFILER_PROFILE_TIMER_DETAIL = Profile_timer(Profiler_thread_inst::get_instance(), (name));
 
 
 struct Profile_event {
-    Profile_event(std::string name,
-            std::thread::id thread_id) : 
-        name(name),
-        start(),
-        end(),
-        thread_id(thread_id) {  }
+        Profile_event(std::string name,
+                        std::thread::id thread_id) : 
+                name(name),
+                start(),
+                end(),
+                thread_id(thread_id) {  }
 
-    std::string name;
-    std::chrono::steady_clock::time_point::rep start;
-    std::chrono::steady_clock::time_point::rep end;
-    std::thread::id thread_id;
+        std::string name;
+        std::chrono::steady_clock::time_point::rep start;
+        std::chrono::steady_clock::time_point::rep end;
+        std::thread::id thread_id;
 };
 
 class Profiler_thread_inst;
 
 class Profiler {
-friend Profiler_thread_inst;
-public:
-    static Profiler& get_instance();
+        friend Profiler_thread_inst;
+        public:
+        static Profiler& get_instance();
 
-    void write_events(const std::vector<Profile_event>& events);
+        void write_events(const std::vector<Profile_event>& events);
 
-    static std::chrono::steady_clock::time_point get_start_point(){
-        return start_point;
-    }
+        static std::chrono::steady_clock::time_point get_start_point(){
+                return start_point;
+        }
 
-private:
-    Profiler();
+        private:
+        Profiler();
 
-    std::mutex io_mut;
-    std::ofstream out_file;
-    std::thread::id pid;
+        std::mutex io_mut;
+        std::ofstream out_file;
+        std::thread::id pid;
 
-    bool active = false;
+        bool active = false;
 
-    static std::chrono::steady_clock::time_point start_point;
+        static std::chrono::steady_clock::time_point start_point;
 };
 
 class Profile_timer;
 
 class Profiler_thread_inst {
-friend Profile_timer;
-public:
+        friend Profile_timer;
+        public:
 
-    Profiler_thread_inst(Profiler& profiler);
-    ~Profiler_thread_inst();
+        Profiler_thread_inst(Profiler& profiler);
+        ~Profiler_thread_inst();
 
-    static Profiler_thread_inst& get_instance();
+        static Profiler_thread_inst& get_instance();
 
-    void add_event(Profile_event&& event);
+        void add_event(Profile_event&& event);
 
-private:
-    Profiler& profiler;
-    std::vector<Profile_event> thread_events;
-    std::chrono::steady_clock::time_point::rep last_ts;
+        private:
+        Profiler& profiler;
+        std::vector<Profile_event> thread_events;
+        std::chrono::steady_clock::time_point::rep last_ts;
 };
 
 class Profile_timer {
-friend Profiler;
-public:
-    Profile_timer(Profiler_thread_inst& profiler, std::string name);
-    ~Profile_timer();
+        friend Profiler;
+        public:
+        Profile_timer(Profiler_thread_inst& profiler, std::string name);
+        ~Profile_timer();
 
-    Profile_timer& operator=(Profile_timer&& rhs);
+        Profile_timer& operator=(Profile_timer&& rhs);
 
-private:
-    void commit();
+        private:
+        void commit();
 
-    Profiler_thread_inst& profiler;
-    Profile_event event;
+        Profiler_thread_inst& profiler;
+        Profile_event event;
 };
 
 #else //BUILD_PROFILED
