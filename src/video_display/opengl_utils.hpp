73ce0804a04097c23761b8e83d08bf66446e9086
Martin Piatka <mapisvk@gmail.com>
2021-01-06 14:38:55 +0100

    opengl_utils: Documentation

--- src/video_display/opengl_utils.hpp
+++ src/video_display/opengl_utils.hpp
@@ -11,11 +11,23 @@
 
 #include "types.h"
 
+/**
+ * RAII wrapper for OpenGL program
+ */
 class GlProgram{
 public:
+        /**
+         * Compiles provided shaders and constructs an OpenGL program
+         *
+         * @param vert_src vertex shader source
+         * @param frag_src fragment shader source
+         */
         GlProgram(const char *vert_src, const char *frag_src);
         ~GlProgram();
 
+        /**
+         * Returns the underlying OpenGL program id
+         */
         GLuint get() { return program; }
 
         GlProgram(const GlProgram&) = delete;
@@ -30,6 +42,9 @@ private:
         GLuint program = 0;
 };
 
+/**
+ * RAII wrapper for OpenGL VBO and VAO containing a 3D model
+ */
 class Model{
 public:
         Model(const Model&) = delete;
@@ -38,11 +53,24 @@ public:
         Model& operator=(Model&& o) { swap(o); return *this; }
         ~Model();
 
+        /**
+         * Return the underlying OpenGL VAO id
+         */
         GLuint get_vao() const { return vao; }
 
+        /**
+         * Renders the contained model by calling glDrawElements or glDrawArrays
+         */
         void render();
 
+        /**
+         * Returns an instance containing a 3D sphere
+         */
         static Model get_sphere();
+
+        /**
+         * Returns an instance containing a plane formed by 2 triangles
+         */
         static Model get_quad();
 
 private:
@@ -59,21 +87,72 @@ private:
         GLsizei indices_num = 0;
 };
 
+/**
+ * RAII wrapper for OpenGL texture object
+ */
 class Texture{
 public:
+        /**
+         * Constructs an instance containing a new OpenGL texture object
+         */
         Texture();
         ~Texture();
 
+        /**
+         * Returns the underlying OpenGL texture id
+         */
         GLuint get() const { return tex_id; }
 
+        /**
+         * Allocates the the storage for the texture according to requested
+         * resolution and format
+         *
+         * @param w width of the image 
+         * @param h height of the image 
+         * @param fmt format of the image (same as internalformat parameter of 
+         * glTexSubImage2D())
+         */
         void allocate(int w, int h, GLenum fmt);
+
+        /**
+         * Uploads image data to the texture
+         *
+         * @param w width of the image 
+         * @param h height of the image 
+         * @param type format of the image (same as type parameter of 
+         * glTexSubImage2D())
+         * @param fmt format of the image (same as internalformat parameter of 
+         * glTexSubImage2D())
+         * @param data pointer to image data to upload
+         * @param data_len length of image data to upload
+         */
         void upload(size_t w, size_t h,
                         GLenum fmt, GLenum type,
                         const void *data, size_t data_len);
+
+        /**
+         * Uploads image data to the texture
+         *
+         * @param w width of the image 
+         * @param h height of the image 
+         * @param type format of the image (same as type parameter of 
+         * glTexSubImage2D())
+         * @param fmt format of the image (same as internalformat parameter of 
+         * glTexSubImage2D())
+         * @param data pointer to image data to upload
+         * @param data_len length of image data to upload
+         */
         void upload_internal_pbo(size_t w, size_t h,
                         GLenum fmt, GLenum type,
                         const void *data, size_t data_len);
 
+        /**
+         * Uploads video frame to the texture
+         *
+         * @param f video frame to upload
+         * @pbo_frame true if the video frame contains the image data
+         * in a PBO buffer
+         */
         void upload_frame(video_frame *f, bool pbo_frame);
 
         Texture(const Texture&) = delete;
@@ -81,6 +160,10 @@ public:
         Texture& operator=(const Texture&) = delete;
         Texture& operator=(Texture&& o) { swap(o); return *this; }
 
+
+        /**
+         * Swaps the contents of two Texture objects
+         */
         void swap(Texture& o){
                 std::swap(tex_id, o.tex_id);
                 std::swap(width, o.width);
@@ -97,8 +180,14 @@ private:
         GLuint pbo;
 };
 
+/**
+ * RAII wrapper for an OpenGL framebuffer object
+ */
 class Framebuffer{
 public:
+        /**
+         * Constructs a new framebuffer object
+         */
         Framebuffer(){
                 glGenFramebuffers(1, &fbo);
         }
@@ -112,10 +201,23 @@ public:
         Framebuffer& operator=(const Framebuffer&) = delete;
         Framebuffer& operator=(Framebuffer&& o) { swap(o); return *this; }
 
+        /**
+         * Returns the underlying OpenGL framebuffer id
+         */
         GLuint get() { return fbo; }
 
+        /**
+         * Attaches a texture to the contained framebuffer
+         *
+         * @param tex Id of texture to attach
+         */
         void attach_texture(GLuint tex);
 
+        /**
+         * Attaches a texture to the contained framebuffer
+         *
+         * @param tex Texture object containg the texture to attach
+         */
         void attach_texture(const Texture& tex){
                 attach_texture(tex.get());
         }
@@ -128,8 +230,14 @@ private:
         GLuint fbo = 0;
 };
 
+/**
+ * RAII wrapper for OpenGL buffer object
+ */
 class GlBuffer{
 public:
+        /**
+         * Constructs a new OpenGL buffer
+         */
         GlBuffer(){
                 glGenBuffers(1, &buf_id);
         }
@@ -138,6 +246,9 @@ public:
                 glDeleteBuffers(1, &buf_id);
         }
 
+        /**
+         * Returns the underlying OpenGL buffer
+         */
         GLuint get() {
                 return buf_id;
         }
@@ -155,12 +266,27 @@ private:
         GLuint buf_id = 0;
 };
 
+/**
+ * Class used to convert YUV textures to RGB textures
+ */
 class Yuv_convertor{
 public:
         Yuv_convertor();
 
+        /**
+         * Renders the video frame containing YUV image data to attached texture
+         *
+         * @param f video frame to convert
+         * @param pbo_frame true if video frame contains the image data
+         * in a PBO buffer
+         */
         void put_frame(video_frame *f, bool pbo_frame = false);
 
+        /**
+         * Attach texture to be used as output
+         *
+         * @param tex texture to attach
+         */
         void attach_texture(const Texture& tex){
                 fbuf.attach_texture(tex);
         }
@@ -172,22 +298,42 @@ private:
         Texture yuv_tex;
 };
 
+/**
+ * Class containing 3 Texture objects to use as a triple buffer.
+ */
 class TripleBufferTexture{
 public:
-
+        /**
+         * Returns a reference to the back texture
+         */
         Texture& get_back(){ return back; }
+
+        /**
+         * Returns a reference to the front texture
+         */
         Texture& get_front(){ return front; }
 
+        /**
+         * Should be called after writing to back texture is finished.
+         * Swaps the back and mid textures and sets a flag indicating that a new
+         * texture is available for reading
+         */
         void swap_back(){
                 back.swap(mid);
                 new_front_available = true;
         }
 
+        /**
+         * Swaps front and mid texture. Unsets the flag indicating a new texture.
+         */
         void swap_front(){
                 front.swap(mid);
                 new_front_available = false;
         }
 
+        /**
+         * Returns the state of the flag indicating a new texture
+         */
         bool is_new_front_available() const { return new_front_available; }
 
 private:
@@ -198,14 +344,49 @@ private:
         bool new_front_available = false;
 };
 
+/**
+ * Class representing the scene to render. Contains the model and texture to
+ * be rendered. Automatically performs pixel format coversions if needed.
+ * It is possible to call put_frame() from another thread, but limitations apply
+ */
 struct Scene{
         Scene();
 
+        /**
+         * Renders the scene.
+         *
+         * @param width Width of result used for aspect ration correction
+         * @param height Height of result used for aspect ration correction
+         */
         void render(int width, int height);
+
+        /**
+         * Renders the scene.
+         *
+         * @param width Width of result used for aspect ration correction
+         * @param height Height of result used for aspect ration correction
+         * @param pvMat custom perspective and view Matrix to be used
+         */
         void render(int width, int height, const glm::mat4& pvMat);
 
+        /**
+         * Uploads a video frame to be rendered. Can be called from other
+         * thread, but must be always called from the thread that called it the
+         * first time.
+         *
+         * @param f video frame to upload
+         * @param pbo_frame true if video frame contains the image data
+         * in a PBO buffer
+         */
         void put_frame(video_frame *f, bool pbo_frame = false);
 
+        /**
+         * Rotate the view. The vertical rotation is constrained to -90 to 90
+         * degrees.
+         *
+         * @param dx horizontal angle to rotate by
+         * @param dy vertical angle to rotate by
+         */
         void rotate(float dx, float dy);
 
         GlProgram program;// = GlProgram(persp_vert_src, persp_frag_src);
@@ -222,8 +403,32 @@ struct Scene{
         int width, height;
 };
 
+/**
+ * RAII wrapper for Sdl window
+ */
 struct Sdl_window{
+        /**
+         * Creates a window using default parameters.
+         *
+         * @param double_buffer Controls the double buffering. If true, the default
+         * is used, if false it is disabled
+         */
         Sdl_window(bool double_buffer = false);
+
+        /**
+         * Creates a window.
+         *
+         * @param title window title
+         * @param x x position of the window, for centering
+         * SDL_WINDOWPOS_CENTERED can be passed
+         * @param y y position of the window, for centering
+         * SDL_WINDOWPOS_CENTERED can be passed
+         * @param w width of window
+         * @param h height of window
+         * @param flags Sdl flags for window creation
+         * @param double_buffer Controls the double buffering. If true, the default
+         * is used, if false it is disabled
+         */
         Sdl_window(const char *title,
                         int x, int y, int w, int h,
                         Uint32 flags, bool double_buffer = false);
@@ -235,16 +440,39 @@ struct Sdl_window{
         Sdl_window& operator=(const Sdl_window&) = delete;
         Sdl_window& operator=(Sdl_window&& o) { swap(o); return *this; }
 
+        /**
+         * Used to obtain Xlib window handles
+         */
         void getXlibHandles(Display  **xDisplay,
                         GLXContext *glxContext,
                         GLXDrawable *glxDrawable);
 
+        /**
+         * Sets window title
+         *
+         * @param title Title to use
+         */
         void set_title(const std::string& title){
                 SDL_SetWindowTitle(sdl_window, title.c_str());
         }
 
+        /**
+         * If using multiple OpenGL contexts for multithreading this function
+         * makes the render context current
+         */
         void make_render_context_current();
+
+        /**
+         * If using multiple OpenGL contexts for multithreading this function
+         * makes the worker context current
+         */
         void make_worker_context_current();
+
+        /**
+         * Returns the underlying worker context object. If it doesn'ŧ yet
+         * exists it gets created. Should be called at least once from the
+         * rendering thread before the worker thread starts.
+         */
         SDL_GLContext get_worker_context();
 
         void swap(Sdl_window& o);
