04d7a2230862f3e6f733b926d0df2b9a3dda10dc
Martin Piatka <piatka@cesnet.cz>
2021-09-20 14:29:27 +0200

    audio/filter: delay audio filter

--- src/audio/filter/delay.cpp
+++ src/audio/filter/delay.cpp
@@ -41,20 +41,48 @@
 #include "config_win32.h"
 #endif /* HAVE_CONFIG_H */
 
+#include <memory>
+
+#include "debug.h"
 #include "audio/audio_filter.h"
+#include "audio/types.h"
 #include "lib_common.h"
+#include "utils/ring_buffer.h"
+
+namespace{
+        struct Ring_buf_deleter{
+                void operator()(ring_buffer_t *ring){ ring_buffer_destroy(ring); }
+        };
+}
 
 struct state_delay{
         int samples;
+        int bps;
+        int ch_count;
+        std::unique_ptr<ring_buffer_t, Ring_buf_deleter> ring;
 };
 
 static int init(const char *cfg, void **state){
         state_delay *s = new state_delay();
-        s->samples = 48000;
+        s->samples = 512;
+        s->bps = 0;
+        s->ch_count = 0;
 
         *state = s;
 
         return 0;
+}
+
+static bool configure_filter(void *state, int ch_count, int bps, int rate){
+        auto s = static_cast<state_delay *>(state);
+
+        s->bps = bps;
+        s->ch_count = ch_count;
+
+        int delay_size = s->bps * s->ch_count * s->samples;
+        s->ring.reset(ring_buffer_init(delay_size * 2));
+        ring_fill(s->ring.get(), 0, delay_size);
+        return true;
 }
 
 static void done(void *state){
@@ -65,6 +93,28 @@ static void done(void *state){
 
 static struct audio_frame *filter(void *state, struct audio_frame *f){
         auto s = static_cast<state_delay *>(state);
+
+        if(f->bps != s->bps || f->ch_count != s->ch_count){
+                configure_filter(state, f->ch_count, f->bps, f->sample_rate);
+        }
+
+        int frame_samples = f->data_len / (f->ch_count * f->bps);
+
+        if(frame_samples <= s->samples){
+                ring_buffer_write(s->ring.get(), f->data, f->data_len);
+                ring_buffer_read(s->ring.get(), f->data, f->data_len);
+        } else {
+                int delay_size = s->bps * s->ch_count * s->samples;
+                int excess_size = (frame_samples - s->samples) * s->bps * s->ch_count;
+
+                ring_buffer_t *ring = s->ring.get();
+
+                ring_buffer_write(ring,
+                                f->data + (f->data_len - delay_size), delay_size);
+
+                memmove(f->data + delay_size, f->data, f->data_len - delay_size);
+                ring_buffer_read(s->ring.get(), f->data, delay_size);
+        }
 
         return f;
 }
