afffaa5e59c70a5d31a129eb135edc80d2efc0a8
Martin Piatka <piatka@cesnet.cz>
2021-10-27 16:26:46 +0200

    RPI4 video_out WIP

--- src/video_display/rpi4_out.cpp
+++ src/video_display/rpi4_out.cpp
@@ -44,40 +44,250 @@
 #include "video.h"
 #include "video_codec.h"
 #include "video_display.h"
+#include "hwaccel_rpi4.h"
+
+#include <memory>
+#include <queue>
+#include <stack>
+#include <mutex>
+#include <condition_variable>
+#include <stdexcept>
+
+#include <bcm_host.h>
+#include <interface/mmal/mmal.h>
+#include <interface/mmal/mmal_component.h>
+#include <interface/mmal/util/mmal_default_components.h>
+
+#define MAX_BUFFER_SIZE 3
+
+namespace{
+
+struct frame_deleter{
+        void operator()(struct video_frame *f){
+                vf_free(f);
+        }
+};
+
+struct mmal_component_deleter{
+        void operator()(MMAL_COMPONENT_T *c){
+                mmal_component_destroy(c);
+        }
+};
+
+using mmal_component_unique = std::unique_ptr<MMAL_COMPONENT_T, mmal_component_deleter>;
+
+struct mmal_pool_deleter{
+        void operator()(MMAL_POOL_T *p){
+                mmal_pool_destroy(p);
+        }
+};
+
+using mmal_pool_unique = std::unique_ptr<MMAL_POOL_T, mmal_pool_deleter>;
+
+class Rpi4_video_out{
+public:
+        Rpi4_video_out() = default;
+        Rpi4_video_out(int x, int y, int width, int height, bool fs, int layer);
+private:
+        void set_output_params();
+
+        int out_pos_x;
+        int out_pos_y;
+        int out_width;
+        int out_height;
+        bool fullscreen;
+        int layer;
+
+        mmal_component_unique renderer_component;
+        mmal_pool_unique pool;
+};
+
+void Rpi4_video_out::set_output_params(){
+        MMAL_DISPLAYREGION_T region = {};
+        region.hdr = {MMAL_PARAMETER_DISPLAYREGION, sizeof(region)};
+        region.set = MMAL_DISPLAY_SET_DEST_RECT
+                | MMAL_DISPLAY_SET_FULLSCREEN
+                | MMAL_DISPLAY_SET_LAYER
+                | MMAL_DISPLAY_SET_ALPHA;
+        region.dest_rect = {out_pos_x, out_pos_y, out_width, out_height};
+        region.fullscreen = fullscreen;
+        region.layer = layer;
+        region.alpha = 0xff;
+
+        mmal_port_parameter_set(renderer_component->input[0], &region.hdr);
+}
+
+Rpi4_video_out::Rpi4_video_out(int x, int y, int width, int height, bool fs, int layer):
+        out_pos_x(x),
+        out_pos_y(y),
+        out_width(width),
+        out_height(height),
+        fullscreen(fs),
+        layer(layer)
+{
+        bcm_host_init();
+
+        MMAL_COMPONENT_T *c = nullptr;
+        if(mmal_component_create(MMAL_COMPONENT_DEFAULT_VIDEO_RENDERER, &c) != MMAL_SUCCESS){
+                throw std::runtime_error("Failed to create renderer component");
+        }
+        renderer_component.reset(c);
+
+        set_output_params();
+
+        if(mmal_component_enable(renderer_component.get()) != MMAL_SUCCESS){
+                throw std::runtime_error("Failed to enable renderer component");
+        }
+
+}
+
+} //anonymous namespace
+
+using unique_frame = std::unique_ptr<struct video_frame, frame_deleter>;
 
 struct rpi4_display_state{
-        struct video_frame *f = nullptr;
-
         struct video_desc current_desc;
+
+        std::mutex frame_queue_mut;
+        std::condition_variable new_frame_ready_cv;
+        std::condition_variable frame_consumed_cv;
+        std::queue<unique_frame> frame_queue;
+
+        std::mutex free_frames_mut;
+        std::stack<unique_frame> free_frames;
+
+        Rpi4_video_out video_out;
 };
 
 static void *display_rpi4_init(struct module *parent, const char *cfg, unsigned int flags)
 {
         rpi4_display_state *s = new rpi4_display_state();
+        s->video_out = Rpi4_video_out(0, 0, 1280, 720, false, 2);
         return s;
 }
 
 static void display_rpi4_done(void *state) {
         auto *s = static_cast<rpi4_display_state *>(state);
 
-        vf_free(s->f);
-
         delete s;
+}
+
+static void frame_data_deleter(struct video_frame *buf){
+        auto wrapper = reinterpret_cast<av_frame_wrapper *>(buf->tiles[0].data);
+
+        av_frame_free(&wrapper->av_frame);
+
+        log_msg(LOG_LEVEL_NOTICE, "RPI frame delete\n");
+
+        delete wrapper;
+}
+
+static struct video_frame *alloc_new_frame(rpi4_display_state *s){
+        auto new_frame = vf_alloc_desc(s->current_desc);
+
+        assert(new_frame->tile_count == 1);
+
+        auto wrapper = new av_frame_wrapper();
+
+        wrapper->av_frame = av_frame_alloc();
+
+        new_frame->tiles[0].data_len = sizeof(av_frame_wrapper);
+        new_frame->tiles[0].data = reinterpret_cast<char *>(wrapper);
+        new_frame->callbacks.recycle = av_frame_wrapper_recycle; 
+        new_frame->callbacks.copy = av_frame_wrapper_copy; 
+        new_frame->callbacks.data_deleter = frame_data_deleter; 
+
+        return new_frame;
 }
 
 static struct video_frame *display_rpi4_getf(void *state) {
         auto *s = static_cast<rpi4_display_state *>(state);
 
-        return s->f;
+        {//lock
+                std::lock_guard lk(s->free_frames_mut);
+
+                while(!s->free_frames.empty()){
+                        unique_frame frame = std::move(s->free_frames.top());
+                        s->free_frames.pop();
+                        if(video_desc_eq(video_desc_from_frame(frame.get()), s->current_desc))
+                        {
+                                return frame.release();
+                        }
+                }
+        }
+
+        auto new_frame = alloc_new_frame(s);
+        return new_frame;
 }
 
 static int display_rpi4_putf(void *state, struct video_frame *frame, int flags)
 {
-        vf_recycle(frame);
+        auto *s = static_cast<rpi4_display_state *>(state);
+
+        if(!frame){
+                std::unique_lock lk(s->frame_queue_mut);
+                s->frame_queue.emplace(frame);
+                lk.unlock();
+                s->new_frame_ready_cv.notify_one();
+                return 0;
+        }
+
+        if (flags == PUTF_DISCARD) {
+                vf_recycle(frame);
+                std::lock_guard(s->free_frames_mut);
+                s->free_frames.emplace(frame);
+                return 0;
+        }
+
+        if (s->frame_queue.size() >= MAX_BUFFER_SIZE && flags == PUTF_NONBLOCK) {
+                log_msg(LOG_LEVEL_VERBOSE, "nonblock putf drop\n");
+                vf_recycle(frame);
+                std::lock_guard(s->free_frames_mut);
+                s->free_frames.emplace(frame);
+                return 1;
+        }
+
+        std::unique_lock lk(s->frame_queue_mut);
+        s->frame_consumed_cv.wait(lk, [s]{return s->frame_queue.size() < MAX_BUFFER_SIZE;});
+        s->frame_queue.emplace(frame);
+        lk.unlock();
+        s->new_frame_ready_cv.notify_one();
+
+        return 0;
 }
 
-static void display_rpi4_run(void *)
+static void display_rpi4_run(void *state)
 {
+        auto *s = static_cast<rpi4_display_state *>(state);
+
+        log_msg(LOG_LEVEL_NOTICE, "RPI4 out run\n");
+
+        bool run = true;
+
+        while(run){
+                std::unique_lock lk(s->frame_queue_mut);
+                s->new_frame_ready_cv.wait(lk, [s] {return s->frame_queue.size() > 0;});
+
+                if (s->frame_queue.size() == 0) {
+                        continue;
+                }
+
+                unique_frame frame = std::move(s->frame_queue.front());
+                s->frame_queue.pop();
+                lk.unlock();
+                s->frame_consumed_cv.notify_one();
+
+                if(!frame){
+                        run = false;
+                        break;
+                }
+
+                auto av_wrap = reinterpret_cast<struct av_frame_wrapper *>(frame->tiles[0].data);
+
+                vf_recycle(frame.get());
+                std::lock_guard(s->free_frames_mut);
+                s->free_frames.push(std::move(frame));
+        }
 }
 
 static int display_rpi4_reconfigure(void *state, struct video_desc desc)
@@ -85,14 +295,7 @@ static int display_rpi4_reconfigure(void *state, struct video_desc desc)
         auto *s = static_cast<rpi4_display_state *>(state);
 
         assert(desc.color_spec == RPI4_8);
-
         s->current_desc = desc;
-        vf_free(s->f);
-        s->f= nullptr;
-
-        s->f = vf_alloc_desc_data(desc);
-
-        memset(s->f->tiles[0].data, 0, s->f->tiles[0].data_len);
 
         return TRUE;
 }
