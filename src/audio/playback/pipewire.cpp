2eab769eaf10f26ffff8b413bed8a2e8dba7d348
Martin Piatka <piatka@cesnet.cz>
2023-03-10 11:39:03 +0100

    aplay/pipewire: Move some code to a common header

--- src/audio/playback/pipewire.cpp
+++ src/audio/playback/pipewire.cpp
@@ -42,124 +42,14 @@
 
 #include <memory>
 
-#include <spa/param/audio/format-utils.h>
-#include <pipewire/pipewire.h>
-
 #include "audio/audio_playback.h"
 #include "audio/types.h"
+#include "audio/pipewire_common.hpp"
 #include "debug.h"
 #include "lib_common.h"
 #include "utils/color_out.h"
 #include "utils/ring_buffer.h"
 #include "utils/string_view_utils.hpp"
-
-template<typename T, auto delete_func>
-struct raii_deleter_helper { void operator()(T *p) { delete_func(p); } };
-template<typename T, auto delete_func>
-using raii_uniq_handle = std::unique_ptr<T, raii_deleter_helper<T, delete_func>>;
-
-template<typename T>
-struct raii_proxy_deleter_helper { void operator()(T *p) { pw_proxy_destroy(reinterpret_cast<pw_proxy *>(p)); } };
-template<typename T>
-using raii_uniq_proxy_handle = std::unique_ptr<T, raii_proxy_deleter_helper<T>>;
-
-using pw_thread_loop_uniq = raii_uniq_handle<pw_thread_loop, pw_thread_loop_destroy>;
-using pw_main_loop_uniq = raii_uniq_handle<pw_main_loop, pw_main_loop_destroy>;
-using pw_stream_uniq = raii_uniq_handle<pw_stream, pw_stream_destroy>;
-using pw_context_uniq = raii_uniq_handle<pw_context, pw_context_destroy>;
-using pw_core_uniq = raii_uniq_handle<pw_core, pw_core_disconnect>;
-using pw_registry_uniq = raii_uniq_proxy_handle<pw_registry>;
-
-struct spa_hook_uniq{
-    spa_hook_uniq(){
-        spa_zero(hook);
-    }
-    ~spa_hook_uniq(){
-        spa_hook_remove(&hook);
-    }
-    spa_hook_uniq(spa_hook_uniq&) = delete;
-    spa_hook_uniq& operator=(spa_hook_uniq&) = delete;
-
-    spa_hook& get() { return hook; }
-
-    spa_hook hook;
-};
-
-class pipewire_thread_loop_lock_guard{
-public:
-    pipewire_thread_loop_lock_guard(pw_thread_loop *loop) : l(loop) {
-        pw_thread_loop_lock(l);
-    }
-    ~pipewire_thread_loop_lock_guard(){
-        pw_thread_loop_unlock(l);
-    }
-    pipewire_thread_loop_lock_guard(pipewire_thread_loop_lock_guard&) = delete;
-    pipewire_thread_loop_lock_guard& operator=(pipewire_thread_loop_lock_guard&) = delete;
-
-private:
-    pw_thread_loop *l;
-};
-
-struct pipewire_init_guard{
-    pipewire_init_guard(){
-        pw_init(nullptr, nullptr);
-    }
-    ~pipewire_init_guard(){
-        pw_deinit();
-    }
-    pipewire_init_guard(pipewire_init_guard&) = delete;
-    pipewire_init_guard& operator=(pipewire_init_guard&) = delete;
-};
-
-struct pipewire_state_common{
-    pipewire_init_guard init_guard;
-
-    pw_thread_loop_uniq pipewire_loop;
-    pw_context_uniq pipewire_context;
-    pw_core_uniq pipewire_core;
-    spa_hook_uniq core_listener;
-
-    int pw_pending_seq = 0;
-    int pw_last_seq = 0;
-};
-
-static void on_core_done(void *data, uint32_t id, int seq)
-{
-    auto s = static_cast<pipewire_state_common *>(data);
-
-    s->pw_last_seq = seq;
-    pw_thread_loop_signal(s->pipewire_loop.get(), false);
-}
-
-static void on_core_error(void *data, uint32_t id, int seq, int res, const char *message)
-{
-    log_msg(LOG_LEVEL_ERROR, "Pipewire error: id:%d seq:%x res:%d msg: %s\n", id, seq, res, message);
-}
-
-static const struct pw_core_events core_events = {
-    PW_VERSION_CORE_EVENTS,
-    .info = nullptr,
-    .done = on_core_done,
-    .ping = nullptr,
-    .error = on_core_error,
-    .remove_id = nullptr,
-    .bound_id = nullptr,
-    .add_mem = nullptr,
-    .remove_mem = nullptr,
-};
-
-static bool initialize_pw_common(pipewire_state_common& s){
-    s.pipewire_loop.reset(pw_thread_loop_new("Playback", nullptr));
-    s.pipewire_context.reset(pw_context_new(pw_thread_loop_get_loop(s.pipewire_loop.get()), nullptr, 0)); //TODO check return
-    s.pipewire_core.reset(pw_context_connect(s.pipewire_context.get(), nullptr, 0));
-
-    pw_core_add_listener(s.pipewire_core.get(), &s.core_listener.get(), &core_events, &s);
-
-    pw_thread_loop_start(s.pipewire_loop.get());
-
-    return true;
-}
-
 
 struct state_pipewire_play{
     pipewire_state_common pw;
