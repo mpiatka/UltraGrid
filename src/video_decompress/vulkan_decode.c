62471c99ea0b10be623815d70f6bad30c2b4446a
ondrej-richtr <on.richtr@seznam.cz>
2024-03-12 13:45:55 +0100

    vulkan loader loading and init instance

--- src/video_decompress/vulkan_decode.c
+++ src/video_decompress/vulkan_decode.c
@@ -15,23 +15,104 @@
 #include "video.h"				//?
 #include "video_decompress.h"
 
+//#include <windows.h> //LoadLibrary
+
+#include "vulkan/vulkan.h"
+//#include "vk_video/vulkan_video_codecs_common.h" //?
+
 
 struct state_vulkan_decompress
 {
 	int test;
+	HMODULE vulkanLib;
+	VkInstance instance;
+	PFN_vkGetInstanceProcAddr loader;
+	VkPhysicalDevice physicalDevice;
 };
 
 static void * vulkan_decompress_init(void)
 {
 	//validation layers, queue, nvidia examples
-	printf("vulkan_decode - init123\n");
+	printf("vulkan_decode - init\n");
+
 	struct state_vulkan_decompress *s = calloc(1, sizeof(struct state_vulkan_decompress));
 	if (!s) return NULL;
-
 	s->test = 13;
-	printf("[vulkan_decode] Vulkan_decompress_done initialized with state '%d'\n", s->test);
 
-	
+	const char vulkan_lib_filename[] = "vulkan-1.dll";
+    HMODULE vulkanLib = LoadLibrary(vulkan_lib_filename);
+	if (vulkanLib == NULL)
+	{
+		printf("[vulkan_decode] Vulkan file '%s' not found!\n", vulkan_lib_filename);
+		free(s);
+		return NULL;
+	}
+	//printf("[vulkan_decode] Vulkan file '%s' loaded.\n", vulkan_lib_filename);
+	s->vulkanLib = vulkanLib;
+
+	const char vulkan_proc_name[] = "vkGetInstanceProcAddr";
+	PFN_vkGetInstanceProcAddr getInstanceProcAddr = (PFN_vkGetInstanceProcAddr)GetProcAddress(vulkanLib, vulkan_proc_name);
+    if (getInstanceProcAddr == NULL) {
+
+		printf("[vulkan_decode] Vulkan procedure '%s' not found!\n", vulkan_proc_name);
+        FreeLibrary(vulkanLib);
+		free(s);
+        return NULL;
+    }
+	s->loader = getInstanceProcAddr;
+
+	VkApplicationInfo appInfo = { 0 };
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "UltraGrid vulkan_decode";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+	VkInstanceCreateInfo createInfo = { 0 };
+	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	createInfo.pApplicationInfo = &appInfo;
+	//createInfo.enabledExtensionCount = 
+	//createInfo.ppEnabledExtensionNames
+	createInfo.enabledLayerCount = 0;
+
+	PFN_vkCreateInstance vkCreateInstance = (PFN_vkCreateInstance)getInstanceProcAddr(NULL, "vkCreateInstance");
+	assert(vkCreateInstance != NULL);
+	int result = vkCreateInstance(&createInfo, NULL, &s->instance);
+	if (result != VK_SUCCESS)
+	{
+		printf("[vulkan_decode] Failed to create vulkan instance! Error: %d\n", result);
+        FreeLibrary(vulkanLib);
+		free(s);
+		return NULL;
+	}
+
+	// checking for extensions
+	PFN_vkEnumerateInstanceExtensionProperties vkEnumerateInstanceExtensionProperties =
+					(PFN_vkEnumerateInstanceExtensionProperties)getInstanceProcAddr(NULL, "vkEnumerateInstanceExtensionProperties");
+	assert(vkEnumerateInstanceExtensionProperties != NULL);
+	uint32_t extensions_count = 0;
+
+	vkEnumerateInstanceExtensionProperties(NULL, &extensions_count, NULL);
+	printf("extensions_count: %d\n", extensions_count);
+
+	VkExtensionProperties *extensions = (VkExtensionProperties*)calloc(extensions_count, sizeof(VkExtensionProperties));
+	if (extensions == NULL)
+	{
+		printf("[vulkan_decode] Failed to allocate array for extensions!\n");
+	}
+	else
+	{
+		vkEnumerateInstanceExtensionProperties(NULL, &extensions_count, extensions);
+
+		for (uint32_t i = 0; i < extensions_count; ++i)
+		{
+			printf("\tExtension: '%s'\n", extensions[i].extensionName);
+		}
+	}
+	free(extensions);
+
+	//TODO device enumeration
 
 	return s;
 }
@@ -40,11 +121,14 @@ static void vulkan_decompress_done(void *state)
 {
 	printf("vulkan_decode - done\n");
 	struct state_vulkan_decompress *s = (struct state_vulkan_decompress *)state;
-	if (s)
-	{
-		printf("[vulkan_decode] Vulkan_decompress_done freeing state '%d'\n", s->test);
-	}
+	if (!s) return;
+
+	printf("[vulkan_decode] Vulkan_decompress_done freeing state '%d'\n", s->test);
 	
+	PFN_vkDestroyInstance vkDestroyInstance = (PFN_vkDestroyInstance)s->loader(s->instance, "vkDestroyInstance");
+	if (vkDestroyInstance != NULL) vkDestroyInstance(s->instance, NULL);
+
+	FreeLibrary(s->vulkanLib);
 	free(s);
 }
 
