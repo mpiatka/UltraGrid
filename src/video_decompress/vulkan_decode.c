0cd807df15ffd49a9c3262ade256fecb2da4ec55
ondrej-richtr <on.richtr@seznam.cz>
2024-03-24 16:46:55 +0100

    added probing and preparation to decompress

--- src/video_decompress/vulkan_decode.c
+++ src/video_decompress/vulkan_decode.c
@@ -25,13 +25,16 @@
 
 static PFN_vkDestroyInstance vkDestroyInstance = NULL;
 static PFN_vkDestroyDevice vkDestroyDevice = NULL;
+static PFN_vkGetPhysicalDeviceVideoCapabilitiesKHR vkGetPhysicalDeviceVideoCapabilitiesKHR = NULL;
 
 static bool load_vulkan_functions_with_instance(PFN_vkGetInstanceProcAddr loader, VkInstance instance)
 {
 	vkDestroyInstance = (PFN_vkDestroyInstance)loader(instance, "vkDestroyInstance");
 	vkDestroyDevice = (PFN_vkDestroyDevice)loader(instance, "vkDestroyDevice");
+	vkGetPhysicalDeviceVideoCapabilitiesKHR = (PFN_vkGetPhysicalDeviceVideoCapabilitiesKHR)
+												loader(instance, "vkGetPhysicalDeviceVideoCapabilitiesKHR");
 
-	return vkDestroyInstance && vkDestroyDevice; 
+	return vkDestroyInstance && vkDestroyDevice && vkGetPhysicalDeviceVideoCapabilitiesKHR;
 }
 
 struct state_vulkan_decompress
@@ -44,12 +47,11 @@ struct state_vulkan_decompress
 	VkQueue decode_queue;
 	VkVideoCodecOperationFlagsKHR queueVideoFlags;
 	VkVideoCodecOperationFlagsKHR codecOperation;
+	bool prepared;
 
 	// UltraGrid
 	//video_desc desc;
-	int rshift;
-	int gshift;
-	int bshift;
+	int rshift, gshift, bshift;
 	int pitch;
 	codec_t out_codec;
 };
@@ -368,7 +370,7 @@ static void vulkan_decompress_done(void *state)
 	free(s);
 }
 
-static VkVideoCodecOperationFlagsKHR codec_to_vulkan_flags(codec_t codec)
+static VkVideoCodecOperationFlagsKHR codec_to_vulkan_flag(codec_t codec)
 {
 	switch(codec)
 	{
@@ -376,6 +378,17 @@ static VkVideoCodecOperationFlagsKHR codec_to_vulkan_flags(codec_t codec)
 		case H265: return VK_VIDEO_CODEC_OPERATION_DECODE_H265_BIT_KHR;
 		//case AV1: return VK_VIDEO_CODEC_OPERATION_DECODE_AV1_BIT_KHR;
 		default: return VK_VIDEO_CODEC_OPERATION_NONE_KHR;
+	}
+}
+
+static codec_t vulkan_flag_to_codec(VkVideoCodecOperationFlagsKHR flag)
+{
+	switch(flag)
+	{
+		case VK_VIDEO_CODEC_OPERATION_DECODE_H264_BIT_KHR: return H264;
+		case VK_VIDEO_CODEC_OPERATION_DECODE_H265_BIT_KHR: return H265;
+		//case VK_VIDEO_CODEC_OPERATION_DECODE_AV1_BIT_KHR: return AV1;
+		default: return VIDEO_CODEC_NONE;
 	}
 }
 
@@ -387,7 +400,7 @@ static bool configure_with(struct state_vulkan_decompress *s, struct video_desc 
 			desc.width, desc.height, spec_name, desc.fps, desc.tile_count);
 
 	s->codecOperation = VK_VIDEO_CODEC_OPERATION_NONE_KHR;
-	VkVideoCodecOperationFlagsKHR videoCodecOperation = codec_to_vulkan_flags(desc.color_spec);
+	VkVideoCodecOperationFlagsKHR videoCodecOperation = codec_to_vulkan_flag(desc.color_spec);
 
 	if (!(s->queueVideoFlags & videoCodecOperation))
 	{
@@ -409,7 +422,15 @@ static int vulkan_decompress_reconfigure(void *state, struct video_desc desc,
 	const char *spec_name = get_codec_name(desc.color_spec);
 	const char *out_name = get_codec_name(out_codec);
 	printf("\tcodec color_spec: '%s', out_codec: '%s'\n", spec_name, out_name);
+	if (out_codec == VIDEO_CODEC_NONE) printf("\tRequested probing.\n");
 
+	if (desc.tile_count != 1)
+	{
+		printf("[vulkan_decode] Tiled video formats are not supported!\n");
+		return false;
+	}
+
+	s->prepared = false; //TODO - freeing resources probably needed when s->prepared == true
 	//s->desc = desc;
 	s->rshift = rshift;
 	s->gshift = gshift;
@@ -448,14 +469,107 @@ static int vulkan_decompress_get_property(void *state, int property, void *val, 
 static int vulkan_decompress_get_priority(codec_t compression, struct pixfmt_desc internal, codec_t ugc)
 {
 	printf("vulkan_decode - get_priority\n");
-	UNUSED(compression);
-	UNUSED(internal);
-	UNUSED(ugc);
 	const char *compression_name = get_codec_name(compression);
 	const char *ugc_name = get_codec_name(ugc);
-	printf("\tcompression: '%s', ugc: '%s'\n", compression_name, ugc_name);
+	printf("\tcompression: '%s', ugc: '%s', pixfmt - depth: %d, subsampling: %d, rgb: %d\n",
+			compression_name, ugc_name, internal.depth, internal.subsampling, internal.rgb ? 1 : 0);
 	
-	return 3; //TODO magic value
+	VkVideoCodecOperationFlagsKHR vulkanFlag = codec_to_vulkan_flag(compression);
+
+	if (vulkanFlag == VK_VIDEO_CODEC_OPERATION_DECODE_H264_BIT_KHR ||
+		vulkanFlag == VK_VIDEO_CODEC_OPERATION_DECODE_H265_BIT_KHR)
+			return 30; //TODO magic value
+
+	return -1; 
+}
+
+static VkVideoChromaSubsamplingFlagBitsKHR subsampling_to_vulkan_flag(int subs)
+{
+	switch((enum subsampling)subs)
+	{
+		case SUBS_420: return VK_VIDEO_CHROMA_SUBSAMPLING_420_BIT_KHR;
+		case SUBS_422: return VK_VIDEO_CHROMA_SUBSAMPLING_422_BIT_KHR;
+		case SUBS_444: return VK_VIDEO_CHROMA_SUBSAMPLING_444_BIT_KHR;
+		case SUBS_4444: return VK_VIDEO_CHROMA_SUBSAMPLING_444_BIT_KHR;
+		default: return VK_VIDEO_CHROMA_SUBSAMPLING_INVALID_KHR;
+	}
+}
+
+static VkVideoComponentBitDepthFlagBitsKHR depth_to_vulkan_flag(int depth)
+{
+	switch(depth)
+	{
+		case 8: return VK_VIDEO_COMPONENT_BIT_DEPTH_8_BIT_KHR;
+		case 10: return VK_VIDEO_COMPONENT_BIT_DEPTH_10_BIT_KHR;
+		case 12: return VK_VIDEO_COMPONENT_BIT_DEPTH_12_BIT_KHR;
+		default: return VK_VIDEO_COMPONENT_BIT_DEPTH_INVALID_KHR;
+	}
+}
+
+static bool prepare(struct state_vulkan_decompress *s)
+{
+	printf("vulkan_decode - prepare\n");
+	assert(!s->prepared); //this function should be called only when decompress is not prepared
+
+	struct pixfmt_desc pf_desc = get_pixfmt_desc(s->out_codec);
+	printf("\tpf_desc - depth: %d, subsampling: %d, rgb: %d, accel_type: %d\n",
+				pf_desc.depth, pf_desc.subsampling, pf_desc.rgb, pf_desc.accel_type);
+	
+	if (!pf_desc.depth) //pixel format description is invalid
+	{
+		printf("[vulkan_decode] Got invalid pixel format!\n");
+		return false;
+	}
+
+	VkVideoChromaSubsamplingFlagsKHR chromaSubsampling = subsampling_to_vulkan_flag(pf_desc.subsampling);
+	if (chromaSubsampling == VK_VIDEO_CHROMA_SUBSAMPLING_INVALID_KHR)
+	{
+		printf("[vulkan_decode] Got unsupported subsampling!\n");
+		return false; //TODO maybe return DECODER_UNSUPP_PIXFMT?
+	}
+
+	VkVideoComponentBitDepthFlagBitsKHR vulkanDepth = depth_to_vulkan_flag(pf_desc.depth);
+	if (vulkanDepth == VK_VIDEO_COMPONENT_BIT_DEPTH_INVALID_KHR)
+	{
+		printf("[vulkan_decode] Got unsupported color channel depth!\n");
+		return false; //TODO maybe return DECODER_UNSUPP_PIXFMT?
+	}
+
+	assert(s->codecOperation != VK_VIDEO_CODEC_OPERATION_NONE_KHR);
+	bool isH264 = s->codecOperation == VK_VIDEO_CODEC_OPERATION_DECODE_H264_BIT_KHR;
+
+	//TODO interlacing and stdProfileIdc in h264/h265
+	VkVideoDecodeH264ProfileInfoKHR h264Profile = { .sType = VK_STRUCTURE_TYPE_VIDEO_DECODE_H264_PROFILE_INFO_KHR };
+	VkVideoDecodeH265ProfileInfoKHR h265Profile = { .sType = VK_STRUCTURE_TYPE_VIDEO_DECODE_H265_PROFILE_INFO_KHR };
+	VkVideoProfileInfoKHR videoProfile = { .sType = VK_STRUCTURE_TYPE_VIDEO_PROFILE_INFO_KHR,
+										   .pNext = isH264 ? (void*)&h264Profile : (void*)&h265Profile,
+										   .videoCodecOperation = s->codecOperation,
+										   .chromaSubsampling = chromaSubsampling,
+										   .lumaBitDepth = vulkanDepth,
+										   .chromaBitDepth = vulkanDepth };
+	
+	VkVideoDecodeH264CapabilitiesKHR h264Capabilites = { .sType = VK_STRUCTURE_TYPE_VIDEO_DECODE_H264_CAPABILITIES_KHR };
+	VkVideoDecodeH265CapabilitiesKHR h265Capabilites = { .sType = VK_STRUCTURE_TYPE_VIDEO_DECODE_H265_CAPABILITIES_KHR };
+	VkVideoDecodeCapabilitiesKHR decodeCapabilities = { .sType = VK_STRUCTURE_TYPE_VIDEO_DECODE_CAPABILITIES_KHR,
+														.pNext = isH264 ? (void*)&h264Capabilites : (void*)&h265Capabilites };
+	VkVideoCapabilitiesKHR videoCapabilities = { .sType = VK_STRUCTURE_TYPE_VIDEO_CAPABILITIES_KHR,
+												 .pNext = (void*)&decodeCapabilities };
+	VkResult result = vkGetPhysicalDeviceVideoCapabilitiesKHR(s->physicalDevice, &videoProfile, &videoCapabilities);
+	if (result != VK_SUCCESS)
+	{
+		printf("[vulkan_decode] Failed to get physical device video capabilities!\n");
+		if (result == VK_ERROR_OUT_OF_HOST_MEMORY) puts("\t- Host out of memory.");
+		else if (result == VK_ERROR_OUT_OF_DEVICE_MEMORY) puts("\t- Device out of memory.");
+		else if (result == VK_ERROR_VIDEO_PICTURE_LAYOUT_NOT_SUPPORTED_KHR) puts("\t- Video picture layout not supported.");
+		else if (result == VK_ERROR_VIDEO_PROFILE_OPERATION_NOT_SUPPORTED_KHR) puts("\t- Video operation not supported.");
+		else if (result == VK_ERROR_VIDEO_PROFILE_FORMAT_NOT_SUPPORTED_KHR) puts("\t- Video format not supported.");
+		else if (result == VK_ERROR_VIDEO_PROFILE_CODEC_NOT_SUPPORTED_KHR) puts("\t- Video codec not supported.");
+		else printf("\t- Vulkan error: %d\n", result);
+		return false;
+	}
+
+	printf("Preparation successful.\n");
+	return true;
 }
 
 static decompress_status vulkan_decompress(void *state, unsigned char *dst, unsigned char *src,
@@ -463,24 +577,45 @@ static decompress_status vulkan_decompress(void *state, unsigned char *dst, unsi
                 struct pixfmt_desc *internal_prop)
 {
 	printf("vulkan_decode - decompress\n");
-	UNUSED(dst);
-	UNUSED(src);
-	UNUSED(src_len);
-	UNUSED(frame_seq);
 	UNUSED(callbacks);
 	UNUSED(internal_prop);
 	struct state_vulkan_decompress *s = (struct state_vulkan_decompress *)state;
-    //TODO
-
-	VkVideoProfileInfoKHR videoProfile = { .sType = VK_STRUCTURE_TYPE_VIDEO_PROFILE_INFO_KHR,
-										   .videoCodecOperation = s->codecOperation, 
-										   .chromaSubsampling = VK_VIDEO_CHROMA_SUBSAMPLING_420_BIT_KHR, //?
-										   .lumaBitDepth = VK_VIDEO_COMPONENT_BIT_DEPTH_INVALID_KHR,	 //TODO - should be same asi .chromaBitDepth
-										   .chromaBitDepth = VK_VIDEO_COMPONENT_BIT_DEPTH_INVALID_KHR }; //TODO
-	UNUSED(videoProfile);
-
+    printf("\tdst: %p, src: %p, src_len: %u, frame_seq: %d\n",
+			dst, src, src_len, frame_seq);
+	
     decompress_status res = DECODER_NO_FRAME;
-    
+
+	if (s->out_codec == VIDEO_CODEC_NONE)
+	{
+		printf("\tProbing...\n");
+		codec_t pf = vulkan_flag_to_codec(s->codecOperation);
+		if (pf == VIDEO_CODEC_NONE)
+		{
+			printf("[vulkan_decode] Unsupported codec operation!\n");
+			return res;
+		}
+		
+		struct pixfmt_desc pf_desc = get_pixfmt_desc(pf);
+
+		//TODO
+		if (pf_desc.rgb)
+		{
+			printf("[vulkan_decode] RGB is not supported!\n");
+			return DECODER_UNSUPP_PIXFMT;
+		}
+
+		*internal_prop = pf_desc;
+		return DECODER_GOT_CODEC;
+	}
+
+	if (!s->prepared) s->prepared = prepare(s);
+	if (!s->prepared)
+	{
+		printf("\tFailed to prepare for decompress.\n");
+		return res;
+	}
+
+	//TODO decompress
     return res;
 };
 
