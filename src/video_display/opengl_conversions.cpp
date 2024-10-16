mpiatka/UltraGrid

commit aa74306ba26e2207012bae88ca4dbb768902f6ee

gl_utils: Handle frames that are in PBO already

src/video_display/opengl_conversions.cpp
@@ -134,7 +134,7 @@ public:
 
         virtual ~Rendering_convertor() {  };
 
-        void attach_texture(const Texture& tex) override final {
+        void attach_texture(Texture& tex) override final {
                 fbuf.attach_texture(tex);
         }
 
@@ -168,12 +168,12 @@ protected:
 class Loading_convertor : public Frame_convertor{
 public:
 
-        void attach_texture(const Texture& tex) override final{
+        void attach_texture(Texture& tex) override final{
                 this->tex = &tex;
         }
 
 protected:
-        const Texture *tex = nullptr;
+        Texture *tex = nullptr;
 };
 
 class Yuv_convertor : public Rendering_convertor{
@@ -184,9 +184,12 @@ public:
 
 private:
         void prepare_input_tex(video_frame *f, bool pbo_frame) override final{
-                input_tex.allocate((f->tiles[0].width + 1) / 2, f->tiles[0].height, GL_RGBA);
-                glBindTexture(GL_TEXTURE_2D, input_tex.get());
-                input_tex.upload_frame(f, pbo_frame);
+                int w = (f->tiles[0].width + 1) / 2;
+                int h = f->tiles[0].height;
+
+                input_tex.load_frame(w, h, GL_RGBA,
+                                GL_RGBA, GL_UNSIGNED_BYTE,
+                                f, pbo_frame);
         }
 };
 
@@ -310,16 +313,11 @@ public:
 
 private:
         void prepare_input_tex(video_frame *f, bool pbo_frame = false) override final{
-                //TODO
                 int w = vc_get_linesize(f->tiles[0].width, v210) / 4;
                 int h = f->tiles[0].height;
-                input_tex.allocate(w, h, GL_RGB10_A2);
-                glBindTexture(GL_TEXTURE_2D, input_tex.get());
-
-                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB10_A2,
-                                w, h, 0,
+                input_tex.load_frame(w, h, GL_RGB10_A2,
                                 GL_RGBA, GL_UNSIGNED_INT_2_10_10_10_REV,
-                                f->tiles[0].data);
+                                f, pbo_frame);
         }
 };
 
@@ -358,16 +356,11 @@ public:
 
 private:
         void prepare_input_tex(video_frame *f, bool pbo_frame = false) override final{
-                //TODO
                 int w = f->tiles[0].width;
                 int h = f->tiles[0].height;
-                input_tex.allocate(w, h, GL_RGBA);
-                glBindTexture(GL_TEXTURE_2D, input_tex.get());
-
-                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA,
-                                w, h, 0,
+                input_tex.load_frame(w, h, GL_RGBA,
                                 GL_RGBA, GL_UNSIGNED_SHORT,
-                                f->tiles[0].data);
+                                f, pbo_frame);
         }
 };
 
@@ -513,8 +506,9 @@ public:
         void put_frame(video_frame *f, bool pbo_frame = false) override{
                 int w = f->tiles[0].width;
                 int h = f->tiles[0].height;
-                glBindTexture(GL_TEXTURE_2D, tex->get());
-                glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, w, h, GL_RGB, GL_UNSIGNED_BYTE, f->tiles[0].data);
+                tex->load_frame(w, h, GL_RGB,
+                                GL_RGB, GL_UNSIGNED_BYTE,
+                                f, pbo_frame);
         }
 };
 
@@ -523,8 +517,9 @@ public:
         void put_frame(video_frame *f, bool pbo_frame = false) override{
                 int w = f->tiles[0].width;
                 int h = f->tiles[0].height;
-                glBindTexture(GL_TEXTURE_2D, tex->get());
-                glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, w, h, GL_RGBA, GL_UNSIGNED_BYTE, f->tiles[0].data);
+                tex->load_frame(w, h, GL_RGBA,
+                                GL_RGBA, GL_UNSIGNED_BYTE,
+                                f, pbo_frame);
         }
 };
 

src/video_display/opengl_utils.cpp
@@ -366,6 +366,36 @@ void Texture::upload_frame(video_frame *f, bool pbo_frame){
         }
 }
 
+void Texture::load_frame(int w, int h,
+                GLint internal_format,
+                GLenum src_format,
+                GLenum type,
+                video_frame *f,
+                bool pbo_frame)
+{
+        char *src_data = f->tiles[0].data;
+        allocate(w, h, internal_format);
+        glBindTexture(GL_TEXTURE_2D, tex_id);
+
+        if(pbo_frame){
+                GlBuffer *pbo = static_cast<GlBuffer *>(f->callbacks.dispose_udata);
+
+                glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo->get());
+                glUnmapBuffer(GL_PIXEL_UNPACK_BUFFER);
+                f->tiles[0].data = nullptr;
+                src_data = nullptr;
+        }
+
+        glTexImage2D(GL_TEXTURE_2D, 0, internal_format,
+                        w, h, 0,
+                        src_format, type,
+                        src_data);
+
+        if(pbo_frame){
+                glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+        }
+}
+
 void Texture::allocate(){
         if(tex_id != 0)
                 return;
@@ -464,6 +494,7 @@ void main(){
         quad = Model::get_quad();
         texture.allocate();
         uploader.attach_dst_texture(&texture);
+        uploader.enable_pbo(false);
 }
 
 void FlatVideoScene::put_frame(video_frame *f){

src/video_display/opengl_utils.hpp
@@ -179,6 +179,13 @@ public:
          */
         void upload_frame(video_frame *f, bool pbo_frame);
 
+        void load_frame(int w, int h,
+                        GLint internal_format,
+                        GLenum src_format,
+                        GLenum type,
+                        video_frame *f,
+                        bool pbo_frame);
+
         Texture(const Texture&) = delete;
         Texture(Texture&& o) { swap(o); }
         Texture& operator=(const Texture&) = delete;
@@ -346,7 +353,7 @@ public:
          *
          * @param tex texture to attach
          */
-        virtual void attach_texture(const Texture& tex) = 0;
+        virtual void attach_texture(Texture& tex) = 0;
 
         void set_pbo(GlBuffer *pbo) { internal_pbo = pbo; }
 protected:

commit fc1967980ae8fdb1a859c1e712fca90fec97cdfb

gl_utils: Internal PBO
Fixes pano_gl performance regression on nvidia introduced in aa74306

src/video_display/opengl_conversions.cpp
@@ -187,10 +187,13 @@ private:
         void prepare_input_tex(video_frame *f, bool pbo_frame) override final{
                 int w = (f->tiles[0].width + 1) / 2;
                 int h = f->tiles[0].height;
+                bool pbo = handle_pbo(f, pbo_frame);
+                char *data = pbo ? nullptr : f->tiles[0].data;
 
-                input_tex.load_frame(w, h, GL_RGBA,
-                                GL_RGBA, GL_UNSIGNED_BYTE,
-                                f, pbo_frame);
+                input_tex.load_frame(w, h, GL_RGBA, GL_RGBA, GL_UNSIGNED_BYTE, data);
+
+                if(pbo)
+                        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
         }
 };
 
@@ -316,9 +319,13 @@ private:
         void prepare_input_tex(video_frame *f, bool pbo_frame = false) override final{
                 int w = vc_get_linesize(f->tiles[0].width, v210) / 4;
                 int h = f->tiles[0].height;
-                input_tex.load_frame(w, h, GL_RGB10_A2,
-                                GL_RGBA, GL_UNSIGNED_INT_2_10_10_10_REV,
-                                f, pbo_frame);
+                bool pbo = handle_pbo(f, pbo_frame);
+                char *data = pbo ? nullptr : f->tiles[0].data;
+
+                input_tex.load_frame(w, h, GL_RGB10_A2, GL_RGBA, GL_UNSIGNED_INT_2_10_10_10_REV, data);
+
+                if(pbo)
+                        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
         }
 };
 
@@ -359,23 +366,33 @@ private:
         void prepare_input_tex(video_frame *f, bool pbo_frame = false) override final{
                 int w = f->tiles[0].width;
                 int h = f->tiles[0].height;
-                input_tex.load_frame(w, h, GL_RGBA,
-                                GL_RGBA, GL_UNSIGNED_SHORT,
-                                f, pbo_frame);
+                bool pbo = handle_pbo(f, pbo_frame);
+                char *data = pbo ? nullptr : f->tiles[0].data;
+
+                input_tex.load_frame(w, h, GL_RGBA, GL_RGBA, GL_UNSIGNED_SHORT, data);
+
+                if(pbo)
+                        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
         }
 };
 
 class DXT1_convertor : public Loading_convertor{
 public:
-        void put_frame(video_frame *f, bool) override{
+        void put_frame(video_frame *f, bool pbo_frame) override{
                 int w = (f->tiles[0].width + 3) / 4 * 4;
                 int h = (f->tiles[0].height + 3) / 4 * 4;
+                bool pbo = handle_pbo(f, pbo_frame);
+                char *data = pbo ? nullptr : f->tiles[0].data;
+
                 glBindTexture(GL_TEXTURE_2D, tex->get());
                 glCompressedTexImage2D(GL_TEXTURE_2D, 0,
                                 GL_COMPRESSED_RGBA_S3TC_DXT1_EXT,
                                 w, h, 0,
                                 w * h / 2,
-                                f->tiles[0].data);
+                                data);
+
+                if(pbo)
+                        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
         }
 };
 
@@ -402,16 +419,22 @@ public:
         DXT5_convertor(): Rendering_convertor(vert_src, fp_display_dxt5ycocg) {  }
 
 private:
-        void prepare_input_tex(video_frame *f, bool) override final{
+        void prepare_input_tex(video_frame *f, bool pbo_frame) override final{
                 int w = (f->tiles[0].width + 3) / 4 * 4;
                 int h = (f->tiles[0].height + 3) / 4 * 4;
+                bool pbo = handle_pbo(f, pbo_frame);
+                char *data = pbo ? nullptr : f->tiles[0].data;
+
                 input_tex.allocate();
                 glBindTexture(GL_TEXTURE_2D, input_tex.get());
                 glCompressedTexImage2D(GL_TEXTURE_2D, 0,
                                 GL_COMPRESSED_RGBA_S3TC_DXT5_EXT,
                                 w, h, 0,
                                 w * h,
-                                f->tiles[0].data);
+                                data);
+
+                if(pbo)
+                        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
         }
 };
 
@@ -442,16 +465,22 @@ public:
         DXT1_YUV_convertor(): Rendering_convertor(vert_src, fp_display_dxt1_yuv) {  }
 
 private:
-        void prepare_input_tex(video_frame *f, bool) override final{
+        void prepare_input_tex(video_frame *f, bool pbo_frame) override final{
                 int w = f->tiles[0].width;
                 int h = f->tiles[0].height;
+                bool pbo = handle_pbo(f, pbo_frame);
+                char *data = pbo ? nullptr : f->tiles[0].data;
+
                 input_tex.allocate();
                 glBindTexture(GL_TEXTURE_2D, input_tex.get());
                 glCompressedTexImage2D(GL_TEXTURE_2D, 0,
                                 GL_COMPRESSED_RGBA_S3TC_DXT1_EXT,
                                 w, h, 0,
                                 (w * h/16) * 8,
-                                f->tiles[0].data);
+                                data);
+
+                if(pbo)
+                        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
         }
 };
 
@@ -507,9 +536,12 @@ public:
         void put_frame(video_frame *f, bool pbo_frame = false) override{
                 int w = f->tiles[0].width;
                 int h = f->tiles[0].height;
-                tex->load_frame(w, h, GL_RGB,
-                                GL_RGB, GL_UNSIGNED_BYTE,
-                                f, pbo_frame);
+                bool pbo = handle_pbo(f, pbo_frame);
+                char *data = pbo ? nullptr : f->tiles[0].data;
+
+                tex->load_frame(w, h, GL_RGB, GL_RGB, GL_UNSIGNED_BYTE, data);
+                if(pbo)
+                        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
         }
 };
 
@@ -518,9 +550,12 @@ public:
         void put_frame(video_frame *f, bool pbo_frame = false) override{
                 int w = f->tiles[0].width;
                 int h = f->tiles[0].height;
-                tex->load_frame(w, h, GL_RGBA,
-                                GL_RGBA, GL_UNSIGNED_BYTE,
-                                f, pbo_frame);
+                bool pbo = handle_pbo(f, pbo_frame);
+                char *data = pbo ? nullptr : f->tiles[0].data;
+
+                tex->load_frame(w, h, GL_RGBA, GL_RGBA, GL_UNSIGNED_BYTE, data);
+                if(pbo)
+                        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
         }
 };
 

src/video_display/opengl_panorama.cpp
@@ -80,6 +80,7 @@ PanoramaScene::PanoramaScene(GlProgram program, Model model): program(std::move(
         model(std::move(model))
 {
         tex.allocateTextures();
+        uploader.enable_pbo(true);
 }
 
 void PanoramaScene::render(int width, int height){

src/video_display/opengl_utils.cpp
@@ -261,30 +261,15 @@ void Texture::load_frame(int w, int h,
                 GLint internal_format,
                 GLenum src_format,
                 GLenum type,
-                video_frame *f,
-                bool pbo_frame)
+                char *data)
 {
-        char *src_data = f->tiles[0].data;
         allocate(w, h, internal_format);
         glBindTexture(GL_TEXTURE_2D, tex_id);
-
-        if(pbo_frame){
-                GlBuffer *pbo = static_cast<GlBuffer *>(f->callbacks.dispose_udata);
-
-                glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo->get());
-                glUnmapBuffer(GL_PIXEL_UNPACK_BUFFER);
-                f->tiles[0].data = nullptr;
-                src_data = nullptr;
-        }
 
         glTexImage2D(GL_TEXTURE_2D, 0, internal_format,
                         w, h, 0,
                         src_format, type,
-                        src_data);
-
-        if(pbo_frame){
-                glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
-        }
+                        data);
 }
 
 void Texture::allocate(){
@@ -314,6 +299,27 @@ void Framebuffer::attach_texture(GLuint tex){
                 printf("Error %d\n", glGetError());
         }
         glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+
+bool Frame_convertor::handle_pbo(video_frame *f, bool pbo_frame){
+        if(pbo_frame){
+                GlBuffer *pbo = static_cast<GlBuffer *>(f->callbacks.dispose_udata);
+
+                glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo->get());
+                glUnmapBuffer(GL_PIXEL_UNPACK_BUFFER);
+                f->tiles[0].data = nullptr;
+                return true;
+        } else if(internal_pbo){
+                glBindBuffer(GL_PIXEL_UNPACK_BUFFER, internal_pbo->get());
+                glBufferData(GL_PIXEL_UNPACK_BUFFER, f->tiles[0].data_len, 0, GL_STREAM_DRAW);
+                void *ptr = glMapBuffer(GL_PIXEL_UNPACK_BUFFER, GL_WRITE_ONLY);
+                memcpy(ptr, f->tiles[0].data, f->tiles[0].data_len);
+                glUnmapBuffer(GL_PIXEL_UNPACK_BUFFER);
+
+                return true;
+        }
+
+        return false;
 }
 
 void FrameUploader::put_frame(video_frame *f, bool pbo_frame){

src/video_display/opengl_utils.hpp
@@ -171,11 +171,10 @@ public:
         void allocate();
 
         void load_frame(int w, int h,
-                        GLint internal_format,
-                        GLenum src_format,
-                        GLenum type,
-                        video_frame *f,
-                        bool pbo_frame);
+                GLint internal_format,
+                GLenum src_format,
+                GLenum type,
+                char *data);
 
         Texture(const Texture&) = delete;
         Texture(Texture&& o) { swap(o); }
@@ -311,6 +310,9 @@ public:
         virtual void attach_texture(Texture& tex) = 0;
 
         void set_pbo(GlBuffer *pbo) { internal_pbo = pbo; }
+
+        bool handle_pbo(video_frame *f, bool pbo_frame);
+
 protected:
         GlBuffer *internal_pbo = nullptr;
 };